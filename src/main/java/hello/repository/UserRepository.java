23a6bf5f036a824448564c2553ee6c59d1c9b1ee
isi.cho@gmail.com <isi.cho@gmail.com>
2016-10-04 11:04:26 +0900

    Refactoring: formatting

--- src/main/java/hello/repository/UserRepository.java
+++ src/main/java/hello/repository/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface UserRepository extends  CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
     List<User> findByName(String name);
 }
